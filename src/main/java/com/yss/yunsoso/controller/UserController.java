06e964520ecc8b7de90762863a29f65e0da54fd3
xxs <631617479@qq.com>
2018-01-09 18:17:12 +0800

    redis 测试

--- src/main/java/com/yss/yunsoso/controller/UserController.java
+++ src/main/java/com/yss/yunsoso/controller/UserController.java
@@ -2,10 +2,15 @@ package com.yss.yunsoso.controller;
 
 import com.yss.yunsoso.dao.YunBeanMapper;
 import com.yss.yunsoso.service.SpiderFacade;
+import com.yss.yunsoso.utils.RedisClient;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
 
@@ -17,6 +22,10 @@ import javax.annotation.Resource;
 public class UserController {
     @Resource
     SpiderFacade spderfaFacadeService;
+    @Resource
+    RedisClient redisClient;
+    @Resource
+    JedisPool jedisPool;
 
     @Resource
     private YunBeanMapper beanMapper;
@@ -32,5 +41,15 @@ public class UserController {
     @RequestMapping(value = "/index")
     public String index() {
         return "/search";
+    }
+
+    @RequestMapping(value = "/redis_test")
+    @ResponseBody
+    public String redis_test() throws Exception {
+        Jedis jedis = jedisPool.getResource();
+        jedis.sadd("incremental","659826","365841");
+        jedis.sadd("incremental","789654","3215674");
+        Boolean incremental = jedis.sismember("incremental", "56");
+        return "";
     }
 }
