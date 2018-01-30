6150544395dd93aff2b06bf8d0f5038460a359ec
631617479@qq.com <Xue.631617479>
2018-01-30 16:08:41 +0800

    新增定时爬虫

--- src/main/java/com/yss/yunsoso/service/RedisFacade.java
+++ src/main/java/com/yss/yunsoso/service/RedisFacade.java
@@ -1,7 +1,13 @@
 package com.yss.yunsoso.service;
 
+import java.util.Set;
+
 public interface RedisFacade{
 
     public Boolean addReidisQueue(String key,String value);
 
+    Set<String> getKeyWordFromRedis(String queue);
+
+    //预约关键字已爬取完毕删除
+    Boolean delKeyWordFromRedis(String queue,String kw);
 }
