26c09037cadcec42ef2622762222f5364c45f403
631617479@qq.com <Xue.631617479>
2018-01-30 14:39:49 +0800

    更新数据库字段

--- src/main/java/com/yss/yunsoso/service/impl/RedisFacadeImpl.java
+++ src/main/java/com/yss/yunsoso/service/impl/RedisFacadeImpl.java
@@ -1,8 +1,26 @@
 package com.yss.yunsoso.service.impl;
 
 import com.yss.yunsoso.service.RedisFacade;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Calendar;
 
 @Service
 public class RedisFacadeImpl implements RedisFacade {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    //前台请求若为为请求kw，则加入队列
+    @Override
+    public Boolean addReidisQueue(String key,String value) {
+
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+        zSetOperations.incrementScore(key, value,1.0);  //权重+1  如果没有该value则生成
+
+        return true;
+    }
 }
